ef1ff9e03210a78c3c1a8ef70928ce560bcf07f7
keyurkanavia <keyur.kanavia@objectedge.com>
2016-06-10 20:24:38 +0530

    Skeleton project first commit

---
+++ OEMP/src/com/oe/services/db/DBServices.java
@@ -0,0 +1,13 @@
+package com.oe.services.db;
+
+/**
+ * Mongo DB connecter class to query/update items.
+ * @author keyur.kanavia
+ *
+ */
+public class DBServices {
+
+	public static void main (String [] args) {
+		
+	}
+}
---
+++ OEMP/src/com/oe/services/rest/ProfileServices.java
@@ -0,0 +1,10 @@
+package com.oe.services.rest;
+
+/**
+ * Rest API class using jax-rs for profile operations.
+ * @author keyur.kanavia
+ *
+ */
+public class ProfileServices {
+
+}
