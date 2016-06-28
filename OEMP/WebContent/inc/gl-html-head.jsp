7c6e42bc130dea9bbb18723790e24db1aae7ca34
deepak.verma256 <deepak.verma@oeis-d42.objectedge.com>
2016-06-28 16:15:40 +0530

    Commit 6 - Cookie Interface.

--- OEMP/WebContent/inc/gl-html-head.jsp
+++ OEMP/WebContent/inc/gl-html-head.jsp
@@ -1,3 +1,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 <link rel="stylesheet" type="text/css" href="css/main.css" />
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<script type = "text/javascript"  src = "js/jquery.cookieInterface.js"></script>
+<title>OE INTERNET PORTAL</title>
---
+++ OEMP/WebContent/js/jquery.cookieInterface.js
@@ -0,0 +1,54 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+
+function checkCookie() {
+    var user=getCookie("username");
+    if(user != ""){
+        alert("Welcome again " + user);
+        return user;
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+           setCookie("username", user, 30);
+           return user;
+       }
+    }
+}
+
+
